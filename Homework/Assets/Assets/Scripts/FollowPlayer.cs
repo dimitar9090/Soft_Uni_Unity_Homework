fa324e5ef7268177ad77560f4544eb7e55d054bd
Dimitar Anguelov <d.angelov9090@gmail.com>
2023-05-17 21:45:23 +0300

    I made 1-8 tasks

--- Homework/Assets/Assets/Scripts/FollowPlayer.cs
+++ Homework/Assets/Assets/Scripts/FollowPlayer.cs
@@ -3,11 +3,25 @@
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    public float height = 4;
+    public float distanceFromObject = 10f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += plane.transform.position + offset;
+        Vector3 desiredPosition = plane.transform.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        //transform.position = smoothPosition;
+
+        transform.LookAt(plane.transform.position);
+
+        //I have my own task too :D camera to be always behind object, but it cannot work if code above is active too
+
+        transform.position = plane.transform.position - plane.transform.forward * distanceFromObject;
+        transform.position = new Vector3(transform.position.x, transform.position.y + height, transform.position.z);
     }
 }
--- Homework/Assets/Assets/Scripts/PlayerController.cs
+++ Homework/Assets/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,46 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 2f;
+    public float rotationSpeed = 50f;
     public float verticalInput;
+    public float horizontalInput;
 
     // Update is called once per frame
-    void FixedUpdate()
+    public void Update()
     {
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Vertical");     //forwardInput
+        horizontalInput = Input.GetAxis("Horizontal"); //sideInput
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        var up = Input.GetKey(KeyCode.UpArrow);
+        var down = Input.GetKey(KeyCode.DownArrow);
+
+        var upRoll = Input.GetKey(KeyCode.Q);
+        var downRoll = Input.GetKey(KeyCode.E);
+
+        if (up)
+        {
+            transform.Rotate(-20 * Time.deltaTime, 0, 0);
+        }
+        else if (down)
+        {
+            transform.Rotate(20 * Time.deltaTime, 0, 0);
+        }
+
+        if (upRoll)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+        else if (downRoll)
+        {
+            transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
+        }
+
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime * horizontalInput);
     }
 }
